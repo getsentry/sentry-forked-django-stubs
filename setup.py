2e434a07d856fc7126a307b6c1cdc9f661bbbd50
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2023-10-31 14:55:43 -0400

    sentry fork

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ extras_require = {
 }
 
 setup(
-    name="django-stubs",
+    name="sentry-forked-django-stubs",
     version="5.2.0",
     description="Mypy stubs for Django",
     long_description=readme,
