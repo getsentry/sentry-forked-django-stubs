efb85b81a0694e70bf591d6dfad3caa35df0afe1
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2023-10-31 16:44:47 -0400

    Update bump-version.sh

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-if ! grep -E '^.*\+sentry[0-9]+' <<< "$2"; then
-    : "version must be D.D.D+sentryD got: $2"
-    exit 1
-fi
 sed -i "s/^    version="'"'".*"'"'",$/    version="'"'"$2"'"'",/" setup.py
